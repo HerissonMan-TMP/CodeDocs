2b8c11fe89d606b7f68eab5d2a6241a23f65c2f0
HerissonMan-TMP <013scrat@gmail.com>
2020-10-10 16:23:53 +0200

    PHP Language added

---
+++ src/docs/doc_php.js
@@ -0,0 +1,13 @@
+var languageName = 'PHP'
+var color = 0xF7E018
+var thumbnail = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/640px-PHP-logo.svg.png'
+var docLink = 'https://www.php.net/docs.php'
+var learningVideoURL = 'https://youtu.be/pWG7ajC_OVo'
+
+module.exports = {
+    languageName: languageName,
+    color: color,
+    thumbnail: thumbnail,
+    docLink: docLink,
+    learningVideoURL: learningVideoURL
+}
