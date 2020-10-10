abe64a93ad6b553f794c6acc7997ebb03454ab3c
HerissonMan-TMP <013scrat@gmail.com>
2020-10-10 16:40:40 +0200

    Edited color for PHP doc

--- src/docs/doc_php.js
+++ src/docs/doc_php.js
@@ -1,5 +1,5 @@
 var languageName = 'PHP'
-var color = 0xF7E018
+var color = 0x787CB4
 var thumbnail = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/640px-PHP-logo.svg.png'
 var docLink = 'https://www.php.net/docs.php'
 var learningVideoURL = 'https://youtu.be/pWG7ajC_OVo'
