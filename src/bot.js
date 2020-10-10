ce735b47fcdf62b1cb9f267c3dc232254101348f
HerissonMan-TMP <013scrat@gmail.com>
2020-10-10 17:21:51 +0200

    Function to keep bot alive

--- src/bot.js
+++ src/bot.js
@@ -9,6 +9,32 @@ const prefix = "docs!"
 const client = new Discord.Client()
 client.login(process.env.BOT_TOKEN)
 
+//Keep bot alive
+var http = require('http'); //importing http
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'https://code-docs.herokuapp.com/',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 2 * 60 * 1000); // load every 20 minutes
+}
+startKeepAlive();
+
+
 client.on('message', function(message) {
     
     //Check if the bot needs to deal with the message.
